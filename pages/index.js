1e9cdd6c556214b4cd8cdc8d0943d20e087ff228
Prasan <42831073+prasan321@users.noreply.github.com>
2020-03-31 18:42:30 +0800

    initial_commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+import useStats from '../utils/useStats';
+import Stats from '../components/Stats';
+import CountrySelector from '../components/CountrySelector';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 align='left'>Covid-19 Latest Stats</h1>
+      <GlobalStyle />
+      <Stats url="https://covid19.mathdro.id/api"></Stats>
+      <CountrySelector></CountrySelector>
+    </div>
+  );
+}
+
