dac82e8268864df1d26f1ee6d5e99e2470e7fa0a
Prasan <42831073+prasan321@users.noreply.github.com>
2020-04-09 18:14:02 +0800

    latest commit

---
+++ components/Stats.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import useStats from '../utils/useStats';
+
+const StatGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+`;
+const StatBlock1 = styled.div`
+  background: #286df7;
+  font-size: 2rem;
+  padding: 2rem;
+  border-radius: 2rem;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  text-align: center;
+`;
+
+const StatBlock2 = styled.div`
+  background: #f73a14;
+  font-size: 2rem;
+  padding: 2rem;
+  border-radius: 2rem;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  text-align: center;
+`;
+
+const StatBlock3 = styled.div`
+  background: #41b317;
+  font-size: 2rem;
+  padding: 2rem;
+  border-radius: 2rem;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  text-align: center;
+`;
+
+export default function Stats({ url }) {
+  const { stats, loading, error } = useStats(url);
+  console.log(stats, loading, error);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+  return (
+    <StatGrid>
+      <StatBlock1>
+        <h3>Confirmed:</h3>
+        <span>{stats.confirmed.value}</span>
+      </StatBlock1>
+      <StatBlock2>
+        <h3>Deaths:</h3>
+        <span>{stats.deaths.value}</span>
+      </StatBlock2>
+      <StatBlock3>
+        <h3>Recovered:</h3>
+        <span>{stats.recovered.value}</span>
+      </StatBlock3>
+    </StatGrid>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+import useStats from '../utils/useStats';
+import Stats from '../components/Stats';
+import CountrySelector from '../components/CountrySelector';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 align='left'>Covid-19 Latest Stats</h1>
+      <GlobalStyle />
+      <Stats url="https://covid19.mathdro.id/api"></Stats>
+      <CountrySelector></CountrySelector>
+      <h1 align='left'>All about Corona Virus | Click below video</h1>
+      <iframe align='center' width="420" height="345" src="https://www.youtube.com/embed/FL_wnudHBwk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+    </div>
+    
+  );
+}
+
